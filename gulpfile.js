a318a62548839b2675552e82d9028d7f84bca9b8
Zacharie Monnet <zacmonnet@gmail.com>
2024-12-18 13:30:44 +0100

    refactor : migrate gulpfile.js to ES6 module syntax

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,21 @@
-const gulp            = require('gulp'),
-      cleanDest       = require('gulp-clean-dest'),
-      sass            = require('gulp-sass'),
-      sourcemaps      = require('gulp-sourcemaps'),
-      connect         = require('gulp-connect'),
-      autoprefixer    = require('gulp-autoprefixer'),
-      gulpif          = require('gulp-if'),
-      plumber         = require('gulp-plumber'),
-      notify          = require('gulp-notify'),
-	  open            = require('gulp-open'),
-	  wrap            = require('gulp-wrap'),
-	  headerComment   = require('gulp-header-comment'),
-	  rename          = require('gulp-rename'),
-	  terser          = require('gulp-terser'),
-	  stripCssComments = require('gulp-strip-css-comments')
+import gulp from 'gulp';
+import cleanDest from 'gulp-clean-dest';
+import gulpSass from 'gulp-sass';
+import dartSass from 'sass';
+import sourcemaps from 'gulp-sourcemaps';
+import connect from 'gulp-connect';
+import autoprefixer from 'gulp-autoprefixer';
+import gulpif from 'gulp-if';
+import plumber from 'gulp-plumber';
+import notify from 'gulp-notify';
+import open from 'gulp-open';
+import wrap from 'gulp-wrap';
+import headerComment from 'gulp-header-comment';
+import rename from 'gulp-rename';
+import terser from 'gulp-terser';
+import stripCssComments from 'gulp-strip-css-comments';
+
+const sass = gulpSass(dartSass);
 
 let TASK_NOTIFICATION = false,
 	LIVE_RELOAD = false
@@ -43,14 +46,18 @@ const buildSass = (minify) => () => {
 			License: <%= pkg.license %>
 			Author: <%= pkg.author %>
 		`))
-		.pipe(gulpif(minify, rename({suffix: '.min'})))
+		.pipe(gulpif(minify, rename({
+			suffix: '.min'
+		})))
 		.pipe(gulpif(minify, sourcemaps.write('.')))
 		// Remove piped files so that in case of an error their old versions don't exist.
 		.pipe(cleanDest('dist'))
 		.pipe(gulp.dest('dist'))
-		.pipe(gulpif(TASK_NOTIFICATION, notify({message: 'SASS built'})))
+		.pipe(gulpif(TASK_NOTIFICATION, notify({
+			message: 'SASS built'
+		})))
 }
-      
+
 gulp.task('sass', buildSass(false))
 gulp.task('sass:min', buildSass(true))
 
@@ -59,17 +66,23 @@ const copyJs = (minify) => () => {
 		.pipe(plumber(plumberErrorHandler))
 		.pipe(cleanDest('dist'))
 		.pipe(gulpif(minify, sourcemaps.init()))
-		.pipe(wrap({ src: './umd.template.txt' }))
+		.pipe(wrap({
+			src: './umd.template.txt'
+		}))
 		.pipe(gulpif(minify, terser()))
 		.pipe(headerComment(`
 			jQuery.skedTape v<%= pkg.version %>
 			License: <%= pkg.license %>
 			Author: <%= pkg.author %>
 		`))
-		.pipe(gulpif(minify, rename({suffix: '.min'})))
+		.pipe(gulpif(minify, rename({
+			suffix: '.min'
+		})))
 		.pipe(gulpif(minify, sourcemaps.write('.')))
 		.pipe(gulp.dest('dist'))
-		.pipe(gulpif(TASK_NOTIFICATION, notify({message: 'JS copied'})))
+		.pipe(gulpif(TASK_NOTIFICATION, notify({
+			message: 'JS copied'
+		})))
 }
 
 gulp.task('copy-js', copyJs(false))
@@ -77,10 +90,10 @@ gulp.task('copy-js:min', copyJs(true))
 
 gulp.task('dist-to-docs', () =>
 	gulp.src(`dist/*`)
-        .pipe(plumber(plumberErrorHandler))
-        .pipe(cleanDest('docs'))
-		.pipe(gulp.dest('docs'))
-		.pipe(gulpif(LIVE_RELOAD, connect.reload()))
+	.pipe(plumber(plumberErrorHandler))
+	.pipe(cleanDest('docs'))
+	.pipe(gulp.dest('docs'))
+	.pipe(gulpif(LIVE_RELOAD, connect.reload()))
 )
 
 gulp.task('watch', () => {
@@ -97,7 +110,9 @@ gulp.task('watch', () => {
 	gulp.watch('src/*.js', gulp.parallel('copy-js', 'copy-js:min'))
 	gulp.watch('dist/*', gulp.parallel('dist-to-docs'))
 	// Open browser
-	gulp.src(__filename).pipe(open({uri: 'http://localhost:8080'}))
+	gulp.src(__filename).pipe(open({
+		uri: 'http://localhost:8080'
+	}))
 })
 
 gulp.task('build', gulp.series(
